5eb29e9cf9a5e3198f1b5f3177e0d8ef1f1d7313
Gajendra Sharma <gajendrasharma0145@gmail.com>
2025-06-11 10:10:47 +0530

    changes in adminjobs

--- Client/src/components/admin/AdminJobs.jsx
+++ Client/src/components/admin/AdminJobs.jsx
@@ -1,46 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../shared/Navbar'
-import { Input } from '../ui/input'
-import { Button } from '../ui/button'
-import { Table } from '../ui/table'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import AdminJobsTable from './AdminJobsTable'
-import useGetAllAdminJobs from '@/hooks/useGetAllAdminJobs'
-import { setSearchJobByText } from '@/redux/jobSlice'
+import React, { useEffect, useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Table } from "../ui/table";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import AdminJobsTable from "./AdminJobsTable";
+import useGetAllAdminJobs from "@/hooks/useGetAllAdminJobs";
+import { setSearchJobByText } from "@/redux/jobSlice";
 
 const AdminJobs = () => {
+  useGetAllAdminJobs();
 
-    useGetAllAdminJobs();
+  const naviagte = useNavigate();
+  const dispatch = useDispatch();
 
-    const naviagte = useNavigate();
-    const dispatch = useDispatch();
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState("")
+  useEffect(() => {
+    dispatch(setSearchJobByText(input));
+  }, [input]);
 
-    useEffect(() => {
-        dispatch(setSearchJobByText(input))
-    }, [input])
-
-    return (
-        <div>
-            <Navbar />
-            <div className=' px-[5%] my-10 '>
-                <div className='flex items-center justify-between my-5'>
-                    <Input
-                        className="w-fit"
-                        placeholder="Filter by name, role"
-                        onChange={(e) => setInput(e.target.value)}
-                    />
-                    <Button className="cursor-pointer " onClick={() => naviagte("/admin/jobs/create")}>Post New Jobs</Button>
-                </div>
-                <AdminJobsTable />
-
-
-            </div>
-
+  return (
+    <div>
+      <Navbar />
+      <div className=" px-[5%] my-10 ">
+        <div className="flex items-center justify-between my-5">
+          <Input
+            className="w-fit"
+            placeholder="Filter by name, role"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button
+            className="cursor-pointer "
+            onClick={() => naviagte("/admin/jobs/create")}
+          >
+            Post New Jobs
+          </Button>
         </div>
-    )
-}
+        <AdminJobsTable />
+      </div>
+    </div>
+  );
+};
 
-export default AdminJobs
+export default AdminJobs;
